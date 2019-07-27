242d0454bfe44aa83046bf366e0197263cae54a3
Valonso <Valonso@kaplan.edu>
2019-07-27 15:22:15 -0500

    Initial Commit

---
+++ DockerRedisApp/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace DockerRedisApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                int visits = GetVisits();
+                await context.Response.WriteAsync($"Number of Visits is: {visits}");
+            });
+        }
+
+        private int GetVisits()
+        {
+            try
+            {
+                ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("192.168.99.100:6379");
+                IDatabase db = redis.GetDatabase();
+
+                int visits = 0;
+                var stVal = db.StringGet("client_visits");
+
+                if (stVal.TryParse(out visits))
+                {
+                    visits++;
+                    db.StringSet("client_visits", visits.ToString());
+                }
+
+                return visits;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.ToString()}");
+                return 0;
+            }
+        }
+    }
+}
---
+++ DockerRedisAppNew/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DockerRedisAppNew.Models;
+using StackExchange.Redis;
+
+namespace DockerRedisAppNew.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.NumberOfVisits = 1;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
